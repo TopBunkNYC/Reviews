73ffc2da394c72f381abcc9a41a286bc2ed1f3f6
David Weinzimmer <dweinzimmer@gmail.com>
2018-11-08 11:01:19 -0500

    Add API testing file with pseudocode

---
+++ spec/api.test.js
@@ -0,0 +1,32 @@
+
+
+describe('API functionality', () => {
+  test('creates a review', async () => {
+    // create a review
+    // retrieve a review and save in variable
+    // delete the review that was added
+    // expect(retrievedReview).toBeDefined()
+  })
+
+  test('reads/retrieves a review', async () => {
+    // retrieve a review and save in variable
+    // expect(retrievedReview). to have the text I expect (look at DB)
+  })
+
+  test('updates a review', async () => {
+    // create a review
+    // update the review text
+    // retrieve the review and save in variable
+    // delete the review that was added
+    // expect(retrievedReview). text to be what I updated it to be
+  })
+
+  test('deletes a review', async () => {
+    // create a review
+    // retrieve a review and save in recordBefore
+    // delete the review that was added
+    // attempt to retrieve same review and save in recordAfter
+    // expect(recordBefore).toBeDefined()
+    // expect(recordAfter). to not exist
+  })
+})
