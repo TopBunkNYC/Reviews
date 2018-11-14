6e5247e743145a4b19b57f1cf9e1b53f3e8e24db
David Weinzimmer <dweinzimmer@gmail.com>
2018-11-14 16:08:17 -0500

    Reset sequences on copied tables

--- build-database/copy-pg.sql
+++ build-database/copy-pg.sql
@@ -15,3 +15,13 @@ COPY topbunk.users
 FROM '/Users/dweinzimmer/Documents/Coding/HackReactor_Course/Reviews/build-database/data-users.csv' 
 WITH (HEADER false, DELIMITER E'\t');
 
+ALTER DATABASE topbunk SET search_path = topbunk, public;
+
+ALTER SEQUENCE reviews_r_id_seq 
+RESTART WITH 10000001;
+
+ALTER SEQUENCE listings_l_id_seq 
+RESTART WITH 10000001;
+
+ALTER SEQUENCE users_u_id_seq 
+RESTART WITH 1000001;
