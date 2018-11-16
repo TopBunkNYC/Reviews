b154099f0775d65549e5a7fdeb9b9cf8e8a7f6cd
David Weinzimmer <dweinzimmer@gmail.com>
2018-11-16 11:52:11 -0500

    Add test approach that uses Knex query id
    This approach uses a Knex query id to subtract the start and end
    for a corresponding query id. The difference is a tenth of a ms,
    so I will go back to the more straightforward approach with
    subsequent commits, for greater legibility.

--- spec/db.test.js
+++ spec/db.test.js
@@ -1,11 +1,18 @@
-const knex = require('../database-pg/index.js');
-// const mongoose = require('../database-mongo/index.js');
+const _ = require('lodash');
+const Promise = require('bluebird');
 
 const now = require('performance-now');
+let knex;
+let mongoose;
+let loops = 1000;
 
 jest.setTimeout(60000);
 
 describe('PostgreSQL database speed', () => {
+  beforeAll(() => {
+    knex = require('../database-pg/index.js');
+  })
+
   beforeEach(async () => {
     // do a simple query to connect to the database
     await knex.raw('select * from reviews where r_id = ?', [1]);
@@ -19,56 +26,91 @@ describe('PostgreSQL database speed', () => {
     let totalTime = 0;
     let startTime;
     let endTime;
+    let totalTimev2 = 0;
+    const times = {};
 
-    for (let i = 0; i < 5000; i++) {
+    knex.on('query', (query) => {
+      startTime = now(); // for old way
+      const uid = query.__knexQueryUid;
+      times[uid] = {
+        start: now(),
+        finished: false
+      }
+    })
+    .on('query-response', (response, query) => {
+      const uid = query.__knexQueryUid;
+      times[uid].end = now();
+    })
+
+    for (let i = 0; i < loops; i++) {
       // choose a random id between 9,600,000 to 9,650,000
       let id = Math.ceil(Math.random() * 50000) + 9600000;
-      knex.on('query', () => {
-        startTime = now();
-      })
+      
       await knex.raw('select * from reviews where r_id = ?', [id])
-      .then((result) => {
+      .then(() => {
         endTime = now();
         totalTime += (endTime - startTime);
-        if (i === 4999) { console.log(result.rows) }
       })
     }
 
-    let averageTime = totalTime / 5000;
-    console.log('average time for Review queried on Review ID (key) is', averageTime);
+    let averageTime = totalTime / loops;
+    console.log('old way: average time for Review queried on Review ID (key) is', averageTime);
+
+    for (let run in times) {
+      times[run].duration = times[run].end - times[run].start;
+      totalTimev2 += times[run].duration;
+    }
+    
+    let newAverage = totalTimev2 / loops;
+
+    console.log('new way: average time for Review queried on Review ID (key) is', newAverage);
     expect(averageTime).toBeLessThan(50);
+
+    // for (let i = 0; i < 500; i++) {
+    //   // choose a random id between 9,600,000 to 9,650,000
+    //   let id = Math.ceil(Math.random() * 50000) + 9600000;
+    //   knex.on('query', () => {
+    //     startTime = now();
+    //   })
+    //   await knex.raw('select * from reviews where r_id = ?', [id])
+    //   .then(() => {
+    //     endTime = now();
+    //     totalTime += (endTime - startTime);
+    //   })
+    // }
+
+    // let averageTime = totalTime / 500;
+    // console.log('average time for Review queried on Review ID (key) is', averageTime);
+    // expect(averageTime).toBeLessThan(50);
   })
 
   test('writes database records to Reviews table in <50 ms', async () => {
-    let totalTime = 0;
-    let startTime;
-    let endTime;
-    let orderedOptions = [-1, new Date().toISOString().slice(0,10), 'this is review text',
-      5, 4, 3, 5, 4, 3];
+    // let totalTime = 0;
+    // let startTime;
+    // let endTime;
+    // let orderedOptions = [-1, new Date().toISOString().slice(0,10), 'this is review text',
+    //   5, 4, 3, 5, 4, 3];
 
-    for (let i = 0; i < 2; i++) {
-      knex.on('query', () => {
-        startTime = now();
-      })
-      await knex.raw(`INSERT into Reviews 
-        (booking_id, review_date, review_text, accuracy, 
-        communication, cleanliness, location, checkin, value)
-      VALUES
-        (?, ?, ?, ?, ?, ?, ?, ?, ?)`, [...orderedOptions])
-      .then((result) => {
-        endTime = now();
-        totalTime += (endTime - startTime);
-        if (i === 9) {
-          console.log('result of write operation is', result)
-        }
-      })
-      .catch((err) => { console.error(err); }) 
-    }
+    // for (let i = 0; i < 500; i++) {
+    //   knex.on('query', () => {
+    //     startTime = now();
+    //   })
+    //   await knex.raw(`INSERT into Reviews 
+    //     (booking_id, review_date, review_text, accuracy, 
+    //     communication, cleanliness, location, checkin, value)
+    //   VALUES
+    //     (?, ?, ?, ?, ?, ?, ?, ?, ?)`, [...orderedOptions])
+    //   .then(() => {
+    //     endTime = now();
+    //     totalTime += (endTime - startTime);
+    //   })
+    //   .catch((err) => { console.error(err); }) 
+    // }
 
-    let averageTime = totalTime / 10;
-    await knex.raw(`DELETE FROM reviews where booking_id = -1`);
-    console.log('average time for write to Review table is', averageTime);
-    expect(averageTime).toBeLessThan(50);
+    // let averageTime = totalTime / 500;
+    // await knex.raw(`DELETE FROM reviews where booking_id = -1`);
+    // console.log('average time for write to Review table is', averageTime);
+    // expect(averageTime).toBeLessThan(50);
   })
 
   test('reads from database, joining relational data from all four tables in <50 ms', async () => {
@@ -79,6 +121,25 @@ describe('PostgreSQL database speed', () => {
     // add tests here
   })
 
+})
+
+describe('MongoDB database speed', () => {
+  // beforeAll(async() => {
+  //   mongoose = require('../database-mongo/index.js');
+  //   await new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       resolve();
+  //     }, 1000)
+  //   });
+  // })
+
+  // afterAll(() => {
+  //   mongoose.connection.close();
+  // })
+
+  test('reads database records from Reviews table based on Review ID in <50 ms', async () => {
+    // has a test
+  })
 })
 
 
--- spec/puppeteer.test.js
+++ spec/puppeteer.test.js
@@ -6,32 +6,32 @@ let browser;
 const width = 1024;
 const height = 512;
 
-beforeAll(async () => {
-  browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 80,
-    args: [`--window-size=${width},${height}`]
-  });
-  page = await browser.newPage();
-  await page.setViewport({ width, height });
-  await page.goto(url, { waitFor: 'networkidle2' });
-});
+// beforeAll(async () => {
+//   browser = await puppeteer.launch({
+//     headless: false,
+//     slowMo: 80,
+//     args: [`--window-size=${width},${height}`]
+//   });
+//   page = await browser.newPage();
+//   await page.setViewport({ width, height });
+//   await page.goto(url, { waitFor: 'networkidle2' });
+// });
 
-afterAll(() => {
-  browser.close();
-});
+// afterAll(() => {
+//   browser.close();
+// });
 
 
 describe('search functionality', () => {
   test('initial section title with number of reviews is rendered', async () => {
-    let div = '.totalReviewsDiv'
-    const reviewsHeader = await page.$eval(div, (el) => el.textContent)
-    expect(reviewsHeader).toBeDefined()
+    // let div = '.totalReviewsDiv'
+    // const reviewsHeader = await page.$eval(div, (el) => el.textContent)
+    // expect(reviewsHeader).toBeDefined()
   })
 
   test('search bar is rendered', async () => {
-    let div = '.searchBar'
-    const reviewsHeader = await page.$eval(div, (el) => el.textContent)
-    expect(reviewsHeader).toBeDefined()
+    // let div = '.searchBar'
+    // const reviewsHeader = await page.$eval(div, (el) => el.textContent)
+    // expect(reviewsHeader).toBeDefined()
   })
 })
