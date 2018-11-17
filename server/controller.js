a906f95e8a0e196f427ba93a2adda68353c102c3
David Weinzimmer <dweinzimmer@gmail.com>
2018-11-16 19:15:56 -0500

    Adjust controller and models to use Knex promises

--- server/controller.js
+++ server/controller.js
@@ -2,38 +2,62 @@ const models = require('./model.js');
 
 module.exports = {
   getAllReviews: (req, res) => {
-    models.getAllReviews(req.query.id, (response) => {
-      res.send(response);
-    });
+    models.getAllReviews(req.query.id)
+    .then((response) => {
+      res.send(response.rows)
+    })
+    .catch((err) => {
+      console.error(err);
+    })
   },
 
   getRatings: (req, res) => {
-    models.getRatings(req.query.id, (response) => {
-      res.send(response);
-    });
+    models.getRatings(req.query.id)
+    .then((response) => {
+      res.send(response.rows)
+    })
+    .catch((err) => {
+      console.error(err);
+    })
   },
 
   getSearch: (req, res) => {
-    models.getSearch(req.query.id, req.query.query, (response) => {
-      res.send(response);
-    });
+    models.getSearch(req.query.id, req.query.query)
+    .then((response) => {
+      res.send(response.rows)
+    })
+    .catch((err) => {
+      console.error(err);
+    })
   },
 
   postReview: (req, res) => {
-    models.postReview(req.body, (response) => {
-      res.send(response);
-    });
+    models.postReview(req.body)
+    .then((response) => {
+      res.send(response.rows)
+    })
+    .catch((err) => {
+      console.error(err);
+    })
   },
 
   editReview: (req, res) => {
-    models.editReview(req.query.r_id, req.query.newReviewText, (response) => {
-      res.send(response);
-    });
+    models.editReview(req.query.r_id, req.query.newReviewText)
+    .then((response) => {
+      res.send(response.rows)
+    })
+    .catch((err) => {
+      console.error(err);
+    })
   },
 
   deleteReview: (req, res) => {
-    models.deleteReview(req.query.r_id, (response) => {
-      res.send(response);
-    });
+    models.deleteReview(req.query.r_id)
+    .then((response) => {
+      res.send(response.rows)
+    })
+    .catch((err) => {
+      console.error(err);
+    })
   }
 };
--- server/model.js
+++ server/model.js
@@ -3,66 +3,66 @@ const knex = require('../database-pg/index.js')
 
 module.exports = {
   getAllReviews: (listingId, callback) => {
-    knex.raw(`SELECT *
+    return knex.raw(`SELECT *
     FROM bookings
     INNER JOIN reviews
     ON (bookings.b_id = reviews.booking_id AND bookings.listing_id = ?)
     INNER JOIN users 
     ON (bookings.user_id = users.u_id)
     ORDER BY reviews.review_date DESC;
-    `,(listingId))
-    .then((response) => {
-      callback(response.rows);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    `,(listingId)); 
+    // .then((response) => {
+    //   callback(response.rows);
+    // })
+    // .catch((err) => {
+    //   console.error(err);
+    // });
   },
 
   getRatings: (listingId, callback) => {  // update
-    knex.raw(`SELECT 
+    return knex.raw(`SELECT 
       AVG(accuracy) AS accuracy, 
       AVG(communication) AS communication, 
       AVG(cleanliness) AS cleanliness, 
       AVG(location) AS location, 
       AVG(checkin) AS checkin, 
       AVG(value) AS value
-      FROM Reviews
-      INNER JOIN Bookings
-      ON Reviews.booking_id = Bookings.b_id
-      LEFT JOIN Users
-      ON Bookings.user_id = Users.u_id
-      WHERE Bookings.listing_id = ?
-    `, (listingId))
-    .then((response) => {
-      callback(response.rows);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+      FROM reviews
+      INNER JOIN bookings
+      ON (reviews.booking_id = bookings.b_id AND bookings.listing_id = ?)
+      INNER JOIN users
+      ON (bookings.user_id = users.u_id)
+    `, (listingId));
+    // .then((response) => {
+    //   callback(response.rows);
+    // })
+    // .catch((err) => {
+    //   console.error(err);
+    // });
   },
 
   getSearch: (listingId, query, callback) => {  // update
-    knex.raw(`SELECT *
-      FROM Reviews
+    return knex.raw(`SELECT *
+      FROM reviews
       INNER JOIN Bookings
-      ON Reviews.booking_id = Bookings.b_id
-      LEFT JOIN Users
-      ON Bookings.user_id = Users.u_id
-      WHERE Bookings.listing_id = ?
-      AND Reviews.review_text LIKE ?
+      ON 
+        (reviews.booking_id = bookings.b_id 
+        AND bookings.listing_id = ? 
+        AND reviews.review_text LIKE ?)
+      INNER JOIN Users
+      ON (bookings.user_id = users.u_id)
       ORDER BY Reviews.review_date DESC;
-    `, (listingId, `"${query}"`))
-    .then((response) => {
-      callback(response.rows);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    `, (listingId, `"${query}"`));
+    // .then((response) => {
+    //   callback(response.rows);
+    // })
+    // .catch((err) => {
+    //   console.error(err);
+    // });
   },
 
   postReview: (options, callback) => { // update
-    var orderedOptions = [
+    let orderedOptions = [
       options.bookingId || Math.floor(Math.random() * 15000000),
       new Date().toISOString().slice(0,10),
       options.reviewText, 
@@ -73,43 +73,43 @@ module.exports = {
       options.checkin || null, 
       options.value || null
     ];
-    knex.raw(`
-      INSERT into Reviews 
+    return knex.raw(`
+      INSERT into reviews 
         (booking_id, review_date, review_text, accuracy, 
         communication, cleanliness, location, checkin, value)
       VALUES
         (?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `, (orderedOptions))
-    .then((response) => {
-      callback(response.rows);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    `, (orderedOptions));
+    // .then((response) => {
+    //   callback(response.rows);
+    // })
+    // .catch((err) => {
+    //   console.error(err);
+    // });
   },
 
   editReview: (reviewId, newReviewText, callback) => { // update
-    knex.raw(`
-      UPDATE Reviews SET review_text = ?
+    return knex.raw(`
+      UPDATE reviews SET review_text = ?
       WHERE r_id = ?;
     `, [newReviewText, reviewId])
-    .then((response) => {
-      callback(response.rows);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    // .then((response) => {
+    //   callback(response.rows);
+    // })
+    // .catch((err) => {
+    //   console.error(err);
+    // });
   },
 
   deleteReview: (reviewId, callback) => { // update
-    knex.raw(`DELETE FROM Reviews 
+    return knex.raw(`DELETE FROM reviews 
       WHERE r_id = ?
     `, [reviewId])
-    .then((response) => {
-      callback(response.rows);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    // .then((response) => {
+    //   callback(response.rows);
+    // })
+    // .catch((err) => {
+    //   console.error(err);
+    // });
   }
 };
